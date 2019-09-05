848984a1543f4f3ddc876af3561e551505b202b3
Dishant Kaushik <dkaushik95@gmail.com>
2019-09-05 09:32:30 -0400

    Initial Commit: Added a rectangle which moves according to Assignment#0

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import * as THREE from 'three'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  componentDidMount() {
+    const scene = new THREE.Scene()
+    const aspectRatio = window.innerWidth / window.innerHeight
+    const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000)
+    const renderer = new THREE.WebGLRenderer()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    // document.body.appendChild(renderer.domElement)
+    this.mount.appendChild(renderer.domElement)
+    const geometry = new THREE.BoxGeometry(500, 10, 10)
+    const material = new THREE.MeshBasicMaterial({
+      color: 0x00ff00,
+      wireframe: true
+    })
+    const cube = new THREE.Mesh(geometry, material)
+    scene.add(cube)
+    const clock = new THREE.Clock(true)
+    camera.position.z = 500
+    var animate = () => {
+      const t = clock.getElapsedTime()
+      requestAnimationFrame(animate)
+      cube.position.x += 0.005 * t
+      cube.position.y += 0.005 * t
+      cube.rotation.y += 0.018 * t
+      renderer.render(scene, camera)
+    }
+    animate()
+  }
+  render() {
+    return (
+      <div
+        ref={ref => {
+          this.mount = ref
+        }}
+      ></div>
+    )
+  }
 }
-
-export default App;
